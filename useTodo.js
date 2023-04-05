902f05be48faed1e5693b6b62de32680aed124a8
VictorManuelLH <victormanuelluishernandez@gmail.com>
2023-04-05 03:28:06 -0600

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,47 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer"
+
+export const useTodos = () => {
+    const init = () => {
+        return JSON.parse(localStorage.getItem('todos')) || [];
+    }
+
+    // const initialState = []
+    
+    const [ todos, dispatch ] = useReducer( todoReducer, [], init )
+    
+    useEffect(() => {
+        localStorage.setItem( 'todos', JSON.stringify( todos ) )
+    }, [todos])
+
+    const handleNewTodo = ( todo ) => {
+        const action = {
+            type: '[TODO] Add todo',
+            payload: todo
+        }
+        dispatch( action );
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove todo',
+            payload: id
+        })
+    }
+    
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: '[TODO] Toggle todo',
+            payload: id
+        })
+    }
+    
+    return {
+        todos,
+        handleDeleteTodo,
+        handleNewTodo,
+        handleToggleTodo,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter( todo => !todo.done ).length
+    }
+}
